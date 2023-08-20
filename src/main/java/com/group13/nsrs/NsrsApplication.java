d40858a8f32be5d1bff54930ffb368f0a6218658
Oreki <2763981847@qq.com>
2023-08-20 09:26:26 +0800

    项目初始化

---
+++ src/main/java/com/group13/nsrs/NsrsApplication.java
@@ -0,0 +1,16 @@
+package com.group13.nsrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Oreki
+ */
+@SpringBootApplication
+public class NsrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group13/nsrs/NsrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group13.nsrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
