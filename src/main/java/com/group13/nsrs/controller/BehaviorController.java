134c741b96edaaa20b214fe811573710a907f196
Oreki <2763981847@qq.com>
2023-08-21 11:35:18 +0800

    完成了用户与文章交互行为相关接口

--- src/main/java/com/group13/nsrs/controller/BehaviorController.java
+++ src/main/java/com/group13/nsrs/controller/BehaviorController.java
@@ -1,9 +1,13 @@
 package com.group13.nsrs.controller;
 
+import com.group13.nsrs.model.dto.CommentSaveDto;
 import com.group13.nsrs.model.entity.Article;
 import com.group13.nsrs.model.vo.BehaviorVo;
+import com.group13.nsrs.model.vo.CommentVo;
 import com.group13.nsrs.service.BehaviorService;
+import com.group13.nsrs.service.CommentService;
 import com.group13.nsrs.util.result.Result;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,6 +19,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/behavior")
+@Api(tags = "用户行为接口")
 public class BehaviorController {
     @Resource
     private BehaviorService behaviorService;
@@ -42,5 +47,18 @@ public class BehaviorController {
     @PostMapping("/view/{articleId}")
     public Result<String> view(@PathVariable Long articleId) {
         return behaviorService.view(articleId);
+    }
+
+    @Resource
+    private CommentService commentService;
+
+    @PostMapping("/comment")
+    public Result<Long> saveComment(@RequestBody CommentSaveDto commentSaveDto) {
+        return commentService.saveComment(commentSaveDto);
+    }
+
+    @GetMapping("/comment/{articleId}")
+    public Result<List<CommentVo>> listCommentsByArticleId(@PathVariable Long articleId) {
+        return commentService.listCommentsByArticleId(articleId);
     }
 }
