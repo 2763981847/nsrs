c65f864429c9b371448f2be09762e93ffa7f41e8
Oreki <2763981847@qq.com>
2023-08-24 10:39:52 +0800

    修改了热门文章点赞相关问题

--- src/main/java/com/group13/nsrs/interceptor/TokenInterceptor.java
+++ src/main/java/com/group13/nsrs/interceptor/TokenInterceptor.java
@@ -1,10 +1,13 @@
 package com.group13.nsrs.interceptor;
 
 import com.group13.nsrs.model.entity.User;
+import com.group13.nsrs.service.UserService;
 import com.group13.nsrs.util.JwtHelper;
 import com.group13.nsrs.util.thread.ThreadLocalUtil;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,7 +15,9 @@ import javax.servlet.http.HttpServletResponse;
  * @author Fu Qiujie
  * @since 2023/8/20
  */
+@Component
 public class TokenInterceptor implements HandlerInterceptor {
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("token");
