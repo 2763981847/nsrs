19b3c8b38e5ead8abe4f59e44ce7124b237fdd39
Oreki <2763981847@qq.com>
2023-08-25 14:11:29 +0800

    项目优化

--- src/main/java/com/group13/nsrs/NsrsApplication.java
+++ src/main/java/com/group13/nsrs/NsrsApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Oreki
@@ -11,10 +12,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @MapperScan("com.group13.nsrs.mapper")
 @EnableAsync
+@EnableScheduling
 public class NsrsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NsrsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NsrsApplication.class, args);
+    }
 
 }
